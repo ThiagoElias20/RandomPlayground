3ffbd26bfdc09c43a2023f7ba2b12550dddf60c5
ThiagoElias20 <contatothiagoelias22@gmail.com>
2024-11-15 17:09:29 -0300

    new Hello with html on php and php info and commentary

---
+++ PHP/Hello/HTMLhello.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP in Html</title>
+</head>
+<body>
+    <?php echo '<p>Testing first Hello, executing locally with php -S localhost:8000</p>'; ?>
+</body>
+</html>
